9fab4bcbbb67a5e0d6ee3817638371d643f7e2c6
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-06-15 21:28:19 -0400

    Added grid sections

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -2,13 +2,17 @@ import React from "react";
 import Navigation from "../components/Navigation/navigation";
 import Top from "../components/Top/Top";
 import Primary from "../components/Primary/primary";
+import Limits from "../components/Limits/limits";
+import Senses from "../components/Senses/senses";
 
 const Main = () => {
     return(
         <div>
             <Navigation/>
             <Top/>
+            <Limits/>
             <Primary/>
+            <Senses/>
            
 
         </div>
