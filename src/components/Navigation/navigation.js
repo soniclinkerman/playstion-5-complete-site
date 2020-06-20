3975193bbf77611ac71e6ae706432bffb9f554f6
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-06-20 14:40:20 -0400

    Added hamburger bar

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,37 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./images/logo.png"
 import "../styles.css"
-const Navigation = () => {
-    return(
-        <header>
-           
-            <nav>
-                <div className="nav-top">
-                <span className="logo"> <img src={logo} alt="sony-logo"/></span>
-                <a href="/" className="sign-in">Sign in</a>
-                <i className="fas fa-search"></i>
+import "../../index.js"
+class Navigation extends Component {
+    constructor(props){
+        super();
+        this.state = {
+            open: true
+        }
+    }
 
-                </div>
-
-               
-                <ul>
-                    <a className="nav-link" href="/">Games<li className="nav-item"></li></a>
-                    <a className="nav-link" href="/">Hardware<li className="nav-item"></li></a>
-                    <a className="nav-link" href="/">Services<li className="nav-item"></li></a>
-                    <a className="nav-link" href="/">News<li className="nav-item"></li></a>
-                    <a className="nav-link" href="/">Shop<li className="nav-item"></li></a>
-                    <a className="nav-link" href="/">Help<li className="nav-item"></li></a>
-                </ul>
-            </nav>
-
-            {/* <div className="other">
-                    <a href="#">My Playstation</a>
-                    <button className="btn btn-secondary">Sign In</button>
+    changeOpen = () => {
+        this.setState({open: !this.state.open})
+    }
+    
+    render(){
+        console.log(this.state.open)
+        return(
+            <header>
+            
+                <nav>
+                    <div className="nav-top">
+                    <span className="logo"> <img src={logo} alt="sony-logo"/></span>
+                    <a href="/" className="sign-in">Sign in</a>
                     <i className="fas fa-search"></i>
-            </div> */}
+                    </div>
 
-        </header>
-    )
+                    <ul className={this.state.open ? "": "nav-list"}>
+                        {this.state.open ? 
+                         <button aria-label="Close navigation" className="open-nav" onClick={this.changeOpen}>&#9776;</button>:
+                         <button aria-label="Close navigation" className="close-nav" onClick={this.changeOpen}>&times;</button>
+                        }
+                        
+                   
+                        <a className={this.state.open ? "nav-link-hide": "nav-link"} href="/">Games<li className="nav-item"></li></a>
+                        <a className={this.state.open ? "nav-link-hide": "nav-link"} href="/">Hardware<li className="nav-item"></li></a>
+                        <a className={this.state.open ? "nav-link-hide": "nav-link"} href="/">Services<li className="nav-item"></li></a>
+                        <a className={this.state.open ? "nav-link-hide": "nav-link"} href="/">News<li className="nav-item"></li></a>
+                        <a className={this.state.open ? "nav-link-hide": "nav-link"} href="/">Shop<li className="nav-item"></li></a>
+                        <a className={this.state.open ? "nav-link-hide": "nav-link"} href="/">Help<li className="nav-item"></li></a>
+                    </ul>
+                </nav>
+
+
+
+            </header>
+        )
+       
+
+        
+    }
+    
+    
 }
 
 export default Navigation
